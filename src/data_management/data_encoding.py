24db1ad9ade4d287a3e6dfe518b2775f66e2cf16
anthano <thanabalasingamanoja@gmail.com>
2025-02-13 16:52:30 +0100

    data encoding added

--- src/data_management/data_encoding.py
+++ src/data_management/data_encoding.py
@@ -23,7 +23,7 @@ def encode_data(df):
     encoded_df = df.copy()
     encoded_df["sex"] = encoded_df["sex"].map({"F": 0, "M": 1})
 
-    variables_to_encode = ["seas_birth", "marital_stat", "smoker"]
+    variables_to_encode = ["seas_birth", "marital_stat", "smoker", "language_skill"]
     for variable in variables_to_encode:
         encoded_df = pd.get_dummies(
             encoded_df, columns=[variable], dtype=pd.Int8Dtype()
@@ -43,10 +43,122 @@ def encode_data(df):
         "fam_hist",
         "alc_dependence",
         "illicit_drugs",
+        "cholesterol",
+        "hypertension",
+        "angina_pectoris",
+        "heartattack",
+        "hypothyroid",
+        "hyperthyroid",
+        "asthma",
+        "copd",
+        "allergies",
+        "osteoporosis",
+        "diabetes",
+        "stomach_ulc",
+        "liv_cir_inf",
+        "neuroderm",
+        "psoriasis",
+        "kidney_fail",
+        "stone",
+        "epilepsy",
+        "migraine",
+        "parkinson",
+        "tbi",
+        "stroke",
+        "eyear",
+        "inf",
+        "cancer",
+        "autoimm",
+        "beh",
     ]
 
     for variable in variables_to_int:
         encoded_df[variable] = _map_yes_no(encoded_df[variable])
+
+    cd_cols = ["cholesterol", "hypertension", "angina_pectoris", "heartattack"]
+    metabolic_cols = ["diabetes"]
+    thyroid_cols = ["hypothyroid", "hyperthyroid"]
+    rheumatological_cols = ["osteoporosis"]
+    lung_cols = ["asthma", "copd"]
+    allergies_cols = ["allergies"]
+    gastro_cols = ["stomach_ulc"]
+    liver_cols = ["liv_cir_inf"]
+    skin_cols = ["neuroderm", "psoriasis"]
+    kidney_cols = ["kidney_fail", "stone"]
+    neuro_cols = ["epilepsy", "migraine", "parkinson", "tbi", "stroke"]
+    eyear_cols = ["eyear"]
+    inf_cols = ["inf"]
+    cancer_cols = ["cancer"]
+    other_cols = ["autoimm", "beh"]
+
+    encoded_df["cardiovascular_disease"] = (encoded_df[cd_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["metabolic_disorder"] = (
+        encoded_df[metabolic_cols].sum(axis=1) > 0
+    ).astype(pd.Int8Dtype())
+    encoded_df["thyroid_disorder"] = (encoded_df[thyroid_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["rheumatological_disorder"] = (
+        encoded_df[rheumatological_cols].sum(axis=1) > 0
+    ).astype(pd.Int8Dtype())
+    encoded_df["lung_disorder"] = (encoded_df[lung_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["allergies"] = (encoded_df[allergies_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["gastrointestinal_disorder"] = (
+        encoded_df[gastro_cols].sum(axis=1) > 0
+    ).astype(pd.Int8Dtype())
+    encoded_df["liver_disease"] = (encoded_df[liver_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["skin_disorder"] = (encoded_df[skin_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["kidney_disorder"] = (encoded_df[kidney_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["neuro_disorder"] = (encoded_df[neuro_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["eyear_disease"] = (encoded_df[eyear_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["inf_disease"] = (encoded_df[inf_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["cancer"] = (encoded_df[cancer_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+    encoded_df["other"] = (encoded_df[other_cols].sum(axis=1) > 0).astype(
+        pd.Int8Dtype()
+    )
+
+    cols_to_drop = sum(
+        [
+            cd_cols,
+            metabolic_cols,
+            thyroid_cols,
+            rheumatological_cols,
+            lung_cols,
+            allergies_cols,
+            gastro_cols,
+            liver_cols,
+            skin_cols,
+            kidney_cols,
+            neuro_cols,
+            eyear_cols,
+            inf_cols,
+            cancer_cols,
+            other_cols,
+        ],
+        [],
+    )  # Start summing from an empty list
+
+    encoded_df = encoded_df.drop(columns=cols_to_drop)
 
     return encoded_df
 
