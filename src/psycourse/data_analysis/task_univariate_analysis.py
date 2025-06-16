5b3dc07c1b4cde00c684cff1fa3a903c9ec7eef8
anthano <thanabalasingamanoja@gmail.com>
2025-06-16 10:32:34 +0200

    diagnosis as covariate for univ regression

--- src/psycourse/data_analysis/task_univariate_analysis.py
+++ src/psycourse/data_analysis/task_univariate_analysis.py
@@ -3,7 +3,9 @@ import pandas as pd
 from psycourse.config import BLD_DATA, BLD_RESULTS, SRC
 from psycourse.data_analysis.univariate_analysis import (
     univariate_lipid_regression,
+    univariate_lipid_regression_cov_diag,
     univariate_prs_regression,
+    univariate_prs_regression_cov_diag,
 )
 
 
@@ -19,8 +21,21 @@ def task_univariate_prs_regression(
     univariate_prs_results.to_pickle(produces)
 
 
+def task_univariate_prs_regression_cov_diag(
+    script_path=SRC / "data_analysis" / "univariate_analysis.py",
+    multimodal_df_path=BLD_DATA / "multimodal_complete_df.pkl",
+    produces=BLD_RESULTS / "univariate_prs_results_cov_diag.pkl",
+):
+    """Perform univariate regression on PRS data with diagnosis as added covariate."""
+
+    data = pd.read_pickle(multimodal_df_path)
+    univariate_prs_results = univariate_prs_regression_cov_diag(data)
+    univariate_prs_results.to_pickle(produces)
+
+
+##### LIPID DATA UNIVARIATE REGRESSION TASKS #####
 task_univariate_lipid_regression_produces = {
-    "top20_lipids": BLD_RESULTS / "univariate_lipid_results top20.pkl",
+    "top20_lipids": BLD_RESULTS / "univariate_lipid_results_top20.pkl",
     "univariate_lipid_results": BLD_RESULTS / "univariate_lipid_results.pkl",
 }
 
@@ -36,3 +51,26 @@ def task_univariate_lipid_regression(
     top20_lipids, univariate_lipid_results = univariate_lipid_regression(data)
     top20_lipids.to_pickle(produces["top20_lipids"])
     univariate_lipid_results.to_pickle(produces["univariate_lipid_results"])
+
+
+### Covariate Diagnosis added to Univariate Lipid Regression ###
+
+task_univariate_lipid_regression_produces_cov_diag = {
+    "top20_lipids_cov_diag": BLD_RESULTS
+    / "univariate_lipid_results_top20_cov_diag.pkl",
+    "univariate_lipid_results_cov_diag": BLD_RESULTS
+    / "univariate_lipid_results_cov_diag.pkl",
+}
+
+
+def task_univariate_lipid_regression_cov_diag(
+    script_path=SRC / "data_analysis" / "univariate_analysis.py",
+    multimodal_df_path=BLD_DATA / "multimodal_complete_df.pkl",
+    produces=task_univariate_lipid_regression_produces_cov_diag,
+):
+    """Perform univariate regression on lipid data."""
+
+    data = pd.read_pickle(multimodal_df_path)
+    top20_lipids, univariate_lipid_results = univariate_lipid_regression_cov_diag(data)
+    top20_lipids.to_pickle(produces["top20_lipids_cov_diag"])
+    univariate_lipid_results.to_pickle(produces["univariate_lipid_results_cov_diag"])
