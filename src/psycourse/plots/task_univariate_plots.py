48d411feaa21f253ef669b235c558fe3f1781562
anthano <thanabalasingamanoja@gmail.com>
2025-06-17 11:18:03 +0200

    univariate plots

---
+++ src/psycourse/plots/task_univariate_plots.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from psycourse.config import BLD_DATA, BLD_RESULTS, SRC
+from psycourse.plots.univariate_plots import (
+    plot_corr_matrix_lipid_top20,
+    plot_corr_matrix_prs,
+    plot_univariate_lipid_regression,
+    plot_univariate_prs_regression,
+)
+
+
+def task_plot_univariate_lipid_regression(
+    script_path=SRC / "plots" / "univariate_plots.py",
+    top20_lipids_path=BLD_RESULTS / "univariate_lipid_results_top20.pkl",
+    produces=BLD_RESULTS / "plots" / "univariate_lipid_regression_plot.png",
+):
+    """Plot the top 20 lipids associated with cluster 5 probability
+    using regression coefficients and FDR values."""
+
+    lipid_top20 = pd.read_pickle(top20_lipids_path)
+    plot_univariate_lipid_regression(lipid_top20)
+
+    # Save the plot
+    plt.savefig(produces, bbox_inches="tight")
+    plt.close()
+
+
+def task_plot_univariate_prs_regression(
+    script_path=SRC / "plots" / "univariate_plots.py",
+    prs_results_path=BLD_RESULTS / "univariate_prs_results.pkl",
+    produces=BLD_RESULTS / "plots" / "univariate_prs_regression_plot.png",
+):
+    """Plot the top 20 lipids associated with cluster 5 probability
+    using regression coefficients and FDR values."""
+
+    prs_results = pd.read_pickle(prs_results_path)
+    plot_univariate_prs_regression(prs_results)
+
+    # Save the plot
+    plt.savefig(produces)
+    plt.close()
+
+
+def task_plot_corr_matrix_lipid_top20(
+    script_path=SRC / "plots" / "univariate_plots.py",
+    multimodal_data_path=BLD_DATA / "multimodal_complete_df.pkl",
+    top20_lipids_path=BLD_RESULTS / "univariate_lipid_results_top20.pkl",
+    produces=BLD_RESULTS / "plots" / "lipid_corr_matrix_top20.png",
+):
+    """Plot the correlation matrix of the top 20 lipids."""
+
+    multimodal_df = pd.read_pickle(multimodal_data_path)
+    lipid_top20 = pd.read_pickle(top20_lipids_path)
+    plot_corr_matrix_lipid_top20(multimodal_df, lipid_top20)
+
+    # Save the plot
+    plt.savefig(produces, bbox_inches="tight")
+    plt.close()
+
+
+def task_plot_corr_matrix_prs(
+    script_path=SRC / "plots" / "univariate_plots.py",
+    multimodal_data_path=BLD_DATA / "multimodal_complete_df.pkl",
+    produces=BLD_RESULTS / "plots" / "prs_corr_matrix.png",
+):
+    """Plot the correlation matrix of the top 20 lipids."""
+
+    multimodal_df = pd.read_pickle(multimodal_data_path)
+    plot_corr_matrix_prs(multimodal_df)
+
+    # Save the plot
+    plt.savefig(produces, bbox_inches="tight")
+    plt.close()
---
+++ src/psycourse/plots/univariate_plots.py
@@ -0,0 +1,129 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+def plot_univariate_lipid_regression(top20):
+    """Plot the top 20 lipids associated with cluster 5 probability using regression
+    coefficients and FDR values.
+    Args:
+        top20 (pd.DataFrame): DataFrame containing the top 20 lipids with columns
+        'coef' and 'FDR'.
+    Returns:
+        None: Displays a plot of the regression coefficients with FDR as color.
+    """
+    # Sort top 20 by FDR again to fix order
+    top20_sorted = top20.sort_values("FDR")
+
+    plt.figure(figsize=(8, 6))
+    sns.set_theme(style="whitegrid")
+
+    # Draw lines
+    for i, row in top20_sorted.iterrows():
+        plt.plot([0, row["coef"]], [i, i], "k-", lw=0.5)
+
+    # Draw points
+    sc = plt.scatter(
+        top20_sorted["coef"],
+        top20_sorted.index,
+        c=-np.log10(top20_sorted["FDR"]),
+        cmap="plasma",
+        s=80,
+        edgecolor="k",
+    )
+
+    plt.axvline(0, color="gray", linestyle="--")
+    plt.xlabel("Regression Coefficient")
+    plt.ylabel("Lipid")
+    plt.title(
+        "Top 20 Lipid Associations with Cluster 5 Probability "
+        "(covariates: age, sex, bmi)"
+    )
+    cbar = plt.colorbar(sc)
+    cbar.set_label("-log10(FDR)")
+    plt.tight_layout()
+    # plt.show()
+
+
+def plot_univariate_prs_regression(prs_results):
+    # Sort PRS by FDR to fix order
+    prs_sorted = prs_results.sort_values("FDR")
+
+    plt.figure(figsize=(8, 6))
+    sns.set_theme(style="whitegrid")
+
+    # Draw lines
+    for i, row in prs_sorted.iterrows():
+        plt.plot([0, row["coef"]], [i, i], "k-", lw=0.5)
+
+    # Draw points
+    sc = plt.scatter(
+        prs_sorted["coef"],
+        prs_sorted.index,
+        c=-np.log10(prs_sorted["FDR"]),
+        cmap="plasma",
+        s=80,
+        edgecolor="k",
+    )
+
+    plt.axvline(0, color="gray", linestyle="--")
+    plt.xlabel("Regression Coefficient")
+    plt.ylabel("PRS")
+    plt.title("PRS Associations with Cluster 5 Probability (covariates: age, sex, bmi)")
+    cbar = plt.colorbar(sc)
+    cbar.set_label("-log10(FDR)")
+    plt.tight_layout()
+    # plt.show()
+
+
+def plot_corr_matrix_lipid_top20(multimodal_df, top20_results_df):
+    """Plot a correlation matrix for the top 20 lipids.
+
+    Args:
+        multimodal_df (pd.DataFrame): DataFrame containing lipidomic data (and more).
+        top20_results_df (pd.DataFrame): DataFrame containing top 20 results with
+        lipids in index.
+
+    Returns:
+        None: Displays a correlation matrix plot.
+    """
+    top20_df = multimodal_df[multimodal_df.columns.intersection(top20_results_df.index)]
+    top20_df_corr_matrix = top20_df.corr()
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(
+        top20_df_corr_matrix,
+        annot=True,
+        fmt=".2f",
+        cmap="coolwarm",
+        square=True,
+        cbar_kws={"shrink": 0.8},
+    )
+    plt.title("Correlation Matrix of Top 20 Lipids")
+    plt.tight_layout()
+    # plt.show()
+
+
+def plot_corr_matrix_prs(multimodal_df):
+    """Plot a correlation matrix for the top 20 lipids.
+
+    Args:
+        multimodal_df (pd.DataFrame): DataFrame containing prs (and more).
+
+    Returns:
+        None: Displays a correlation matrix plot.
+    """
+    prs_columns = [col for col in multimodal_df.columns if col.endswith("PRS")]
+    prs_df = multimodal_df[prs_columns]
+    top20_df_corr_matrix = prs_df.corr()
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(
+        top20_df_corr_matrix,
+        annot=True,
+        fmt=".2f",
+        cmap="coolwarm",
+        square=True,
+        cbar_kws={"shrink": 0.8},
+    )
+    plt.title("Correlation Matrix of PRS")
+    plt.tight_layout()
+    # plt.show()
