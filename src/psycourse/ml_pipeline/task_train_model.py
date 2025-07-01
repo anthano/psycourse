87a8ee5180e771eb12ad035e1846ca24a7579bf8
anthano <thanabalasingamanoja@gmail.com>
2025-07-01 11:52:47 +0200

    fixed pytask svm model output - now it is produced by pytask and not produced in function

--- src/psycourse/ml_pipeline/task_train_model.py
+++ src/psycourse/ml_pipeline/task_train_model.py
@@ -1,16 +1,26 @@
+import pickle
+
 import pandas as pd
 
-from psycourse.config import BLD_DATA, SRC
+from psycourse.config import BLD_DATA, BLD_MODELS, SRC
 from psycourse.ml_pipeline.train_model import svm_model
+
+task_products = {
+    "probabilities": BLD_DATA / "svm_predicted_probabilities.pkl",
+    "model": BLD_MODELS / "svm_classifier.pkl",
+}
 
 
 def task_train_model(
     script_path=SRC / "ml_pipeline" / "train_model.py",
     data_path=BLD_DATA / "sparse_dataset_with_targets.pkl",
-    produces=BLD_DATA / "svm_predicted_probabilities.pkl",
+    produces=task_products,
 ):
     """Train SVM Model and save the predicted probabilities."""
 
     data = pd.read_pickle(data_path)
-    cluster_probability_scores_df = svm_model(data)
-    cluster_probability_scores_df.to_pickle(produces)
+    final_model, cluster_probability_scores_df = svm_model(data)
+    cluster_probability_scores_df.to_pickle(task_products["probabilities"])
+
+    with open(task_products["model"], "wb") as f:
+        pickle.dump(final_model, f)
