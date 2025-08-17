24ab943df7717d2d52f68a09c96af74171090104
anthano <thanabalasingamanoja@gmail.com>
2025-08-17 12:53:03 +0200

    update hurdle task for top20 features

--- src/psycourse/data_analysis/task_two_step_hurdle.py
+++ src/psycourse/data_analysis/task_two_step_hurdle.py
@@ -10,22 +10,15 @@ from psycourse.data_analysis.hurdle_wrappers import (
     run_hurdle_analysis,
 )
 
-# TODO: UPDATE ALL THIS WITH BETTER VARIABLES, CLEANER PATHS ETC
-
 DATA = BLD_DATA / "multimodal_complete_df.pkl"
 HURDLE_OUTPUT_DIRECTORY = BLD_RESULTS / "multivariate" / "hurdle_runs"
 BLD_HURDLE = BLD_RESULTS / "multivariate" / "hurdle_runs"
 
 
-# CUTOFFS = [0.05, 0.10, 0.15, 0.25]
-# INNERS = [5, 7, 10]
-# OUTERS = [5, 7, 10]
-# N_REPEATS = 10
-
-CUTOFFS = [0.05, 0.10]
-INNERS = [2]
-OUTERS = [2]
-N_REPEATS = 2
+CUTOFFS = [0.05, 0.10, 0.15, 0.25]
+INNERS = [5, 7, 10]
+OUTERS = [5, 7, 10]
+N_REPEATS = 10
 
 for cutoff, inner, outer in itertools.product(CUTOFFS, INNERS, OUTERS):
     combination_id = f"{cutoff=}_{inner=}_{outer=}"
@@ -61,25 +54,22 @@ for cutoff, inner, outer in itertools.product(CUTOFFS, INNERS, OUTERS):
         clf_top20_df.to_pickle(produces["clf_top20_df"])
         reg_top20_df.to_pickle(produces["reg_top20_df"])
 
+    # ---------------- aggregation per combination (putting all repeats together) ------
+    summary_metrics_path: dict[str, Path] = {
+        "metrics_summary_df": BLD_HURDLE / f"summary__{combination_id}.pkl",
+        "clf_top20_summary_df": BLD_HURDLE / f"clf_top20_summary__{combination_id}.pkl",
+        "reg_top20_summary_df": BLD_HURDLE / f"reg_top20_summary__{combination_id}.pkl",
+    }
 
-# ---------------- aggregation per combination (putting all repeats together) ----------
-for cutoff, inner, outer in product(CUTOFFS, INNERS, OUTERS):
-    combination_id = f"{cutoff=}_{inner=}_{outer=}"
-    metrics_df_path = BLD_HURDLE / f"metrics_df__{combination_id}.pkl"
-    summary_metrics_path = BLD_HURDLE / f"summary__{combination_id}.pkl"
-
-    @task(id=combination_id + "_aggregate")
+    @task(id=combination_id)
     def task_hurdle_aggregate(
-        depends_on: Path = metrics_df_path,
-        produces: Path = summary_metrics_path,
+        depends_on: dict[str, Path] = HURDLE_ANALYSIS_RESULTS_PATHS,
+        produces: dict[str, Path] = summary_metrics_path,
         cutoff: float = cutoff,
         inner: int = inner,
         outer: int = outer,
     ):
-        metrics_df = pd.read_pickle(depends_on).T
-
-        def mean_std(col):
-            return float(metrics_df[col].mean()), float(metrics_df[col].std(ddof=1))
+        metrics_df = pd.read_pickle(depends_on["metrics_df"])
 
         summary = {
             "combo_id": f"{cutoff}_{inner}_{outer}",
@@ -104,9 +94,20 @@ for cutoff, inner, outer in product(CUTOFFS, INNERS, OUTERS):
             "test_regression_mae",
             "test_regression_rmse",
         ]:
-            summary[f"{col}_mean"], summary[f"{col}_std"] = mean_std(col)
+            summary[f"{col}_mean"] = metrics_df[col].mean()
+            summary[f"{col}_std"] = metrics_df[col].std()
 
-        pd.DataFrame([summary]).to_pickle(produces)
+        pd.DataFrame([summary]).to_pickle(produces["metrics_summary_df"])
+
+        clf_top20_df = pd.read_pickle(depends_on["clf_top20_df"])
+        clf_top20_summary_df = clf_top20_df.groupby("feature").agg("mean")
+        clf_top20_summary_df["feature_count"] = clf_top20_df["feature"].value_counts()
+        clf_top20_summary_df.to_pickle(produces["clf_top20_summary_df"])
+
+        reg_top20_df = pd.read_pickle(depends_on["reg_top20_df"])
+        reg_top20_summary_df = reg_top20_df.groupby("feature").agg("mean")
+        reg_top20_summary_df["feature_count"] = reg_top20_df["feature"].value_counts()
+        reg_top20_summary_df.to_pickle(produces["reg_top20_summary_df"])
 
 ## ---------------- final collection across all combinations ----------------
 
@@ -116,7 +117,6 @@ FINAL_SUMMARY_DEPENDS_ON = [
 ]
 
 
-@task(id="final_collection")
 def task_final_collection(
     depends_on: list[Path] = FINAL_SUMMARY_DEPENDS_ON,
     produces: Path = BLD_HURDLE / "final_summary.pkl",
