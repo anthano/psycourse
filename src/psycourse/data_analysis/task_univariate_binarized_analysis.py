69b82f0baee76f11d0a5079ca2e15a99d88115ff
anthano <thanabalasingamanoja@gmail.com>
2025-11-03 13:37:16 +0100

    univariate binarized analysis for lipids

--- src/psycourse/data_analysis/task_univariate_binarized_analysis.py
+++ src/psycourse/data_analysis/task_univariate_binarized_analysis.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
 from psycourse.config import BLD_DATA, BLD_RESULTS, SRC
-from psycourse.data_analysis.univariate_binarized_analysis import univariate_prs_ancova
+from psycourse.data_analysis.univariate_binarized_analysis import (
+    univariate_lipids_ancova,
+    univariate_prs_ancova,
+)
 
 UNIVARIATE_BINARIZED_ANALYSIS_DIR = (
     BLD_RESULTS / "results" / "univariate" / "binarized_analysis"
 )
+
+# ======================================================================================
+# PRS
+# ======================================================================================
 
 
 def task_univariate_prs_ancova(
@@ -14,6 +21,32 @@ def task_univariate_prs_ancova(
     produces=UNIVARIATE_BINARIZED_ANALYSIS_DIR / "univariate_prs_ancova_results.pkl",
 ):
     """Perform univariate PRS ANCOVA with binarized predicted labels (G5 vs. others)."""
+
     multimodal_df = pd.read_pickle(multimodal_data_path)
     prs_ancova_results = univariate_prs_ancova(multimodal_df)
     prs_ancova_results.to_pickle(produces)
+
+
+# ======================================================================================
+# Lipids
+# ======================================================================================
+
+lipid_ancova_products = {
+    "lipid_ancova_results": UNIVARIATE_BINARIZED_ANALYSIS_DIR
+    / "univariate_lipids_ancova_results.pkl",
+    "lipid_ancova_top20_results": UNIVARIATE_BINARIZED_ANALYSIS_DIR
+    / "univariate_lipids_ancova_top20_results.pkl",
+}
+
+
+def task_univariate_lipids_ancova(
+    script_path=SRC / "data_analysis" / "univariate_binarized_analysis.py",
+    multimodal_data_path=BLD_DATA / "multimodal_complete_df.pkl",
+    produces=UNIVARIATE_BINARIZED_ANALYSIS_DIR / "univariate_lipids_ancova_results.pkl",
+):
+    """Perform univariate lipids ANCOVA with binarized predicted labels."""
+
+    multimodal_df = pd.read_pickle(multimodal_data_path)
+    lipids_ancova_results, top20 = univariate_lipids_ancova(multimodal_df)
+    lipids_ancova_results.to_pickle(lipid_ancova_products["lipid_ancova_results"])
+    top20.to_pickle(lipid_ancova_products["lipid_ancova_top20_results"])
