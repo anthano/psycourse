56bee3404a40ec5168fb7998c729015df5faf7e9
anthano <thanabalasingamanoja@gmail.com>
2026-01-16 15:28:14 +1100

    update diablo model

--- src/psycourse/integrated_analysis/diablo_model.R
+++ src/psycourse/integrated_analysis/diablo_model.R
@@ -1,20 +1,57 @@
 library(mixOmics)
-BLD_DATA <- "/Users/anojat/Documents/psycourse/bld/data"
+BLD_DATA <- "/Users/anojat/Documents/psycourse/bld/data/integrated_analysis"
+
 lipid_data <- read.csv(
-  file.path(BLD_DATA, "integrated_analysis_lipid_data.csv")
-)
+  file.path(BLD_DATA, "integrated_analysis_lipid_df_train.csv"),
+            check.names=FALSE)
 prs_data <- read.csv(
-  file.path(BLD_DATA, "integrated_analysis_prs_data.csv")
-)
+  file.path(BLD_DATA, "integrated_analysis_prs_df_train.csv"),
+            check.names=FALSE)
+
 outcome_data <- read.csv(
-  file.path(BLD_DATA, "integrated_analysis_outcome_data.csv")
-)
+  file.path(BLD_DATA, "integrated_analysis_outcome_df_train.csv"),
+            check.names=FALSE)
 
-data <- list(lipid_data,
-             prs_data)
+rownames(lipid_data) <- lipid_data[[1]]
+lipid_data[[1]] <- NULL
 
-lapply(data, dim)
+rownames(prs_data) <- prs_data[[1]]
+prs_data[[1]] <- NULL
+
+rownames(outcome_data) <- outcome_data[[1]]
+outcome_data[[1]] <- NULL
+
+X <- list(
+  lipids = as.matrix(lipid_data),
+  prs = as.matrix(prs_data))
+
+sapply(X, dim)
 
 Y = outcome_data
+Y$true_label <- as.factor(Y$true_label)
+Y_bin <- Y$true_label
+summary(Y)
 
-## NOTE: Do train-test split + set index or sth
+
+# -----------------------------
+# (Optional but recommended for sanity) Explore pairwise integration first
+#    sPLS between PRS and lipids (unsupervised wrt outcome)
+# -----------------------------
+expl_keepX <- 10   # PRS (17 total)
+expl_keepY <- 50   # lipids (361 total)
+
+spls_expl <- spls(X$prs, X$lipids, ncomp = 1, keepX = expl_keepX, keepY = expl_keepY)
+cor(spls_expl$variates$X, spls_expl$variates$Y)
+
+# ----------------------------
+# Here's the thing with the interpretability/shared axis vs. prediction thing.
+# I choose 1 (in a range from [0,1,1]) because I want to optimise cross-corr
+
+design <- matrix(c(0, 1,
+                   1, 0),
+                 nrow = 2, byrow = TRUE)
+colnames(design) <- rownames(design) <- names(X)
+
+ncomp <- 1
+
+# --- here i guess the modeling part starts, not reviewed yet
