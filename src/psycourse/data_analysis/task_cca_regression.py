fef953b6f89713b78be83adcd69ba9943ff7bd63
anthano <thanabalasingamanoja@gmail.com>
2026-01-19 11:52:49 +1100

    cca + regression + permutation for prs and lipids

---
+++ src/psycourse/data_analysis/task_cca_regression.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+from typing import Annotated
+
+import pandas as pd
+from pytask import Product, task
+
+from psycourse.config import BLD_DATA, BLD_RESULTS
+from psycourse.data_analysis.cca_regression import cca_prs_lipids_regression
+
+RESULTS_DIR = BLD_RESULTS / "cca_regression"
+
+prs_cols = [
+    "BD_PRS",
+    "SCZ_PRS",
+    "MDD_PRS",
+    "ADHD_PRS",
+    "Alzheimer_PRS",
+    "Education_PRS",
+    "Agreeableness_PRS",
+    "Conscientiousness_PRS",
+    "Extraversion_PRS",
+    "Neuroticism_PRS",
+    "Openness_PRS",
+    "SleepDurationLong_PRS",
+    "SleepDurationShort_PRS",
+]
+lipid_cols = [
+    "class_LPE",
+    "class_PC",
+    "class_PC_O",
+    "class_PC_P",
+    "class_PE",
+    "class_PE_P",
+    "class_TAG",
+    "class_dCer",
+    "class_dSM",
+    "class_CAR",
+    "class_CE",
+    "class_DAG",
+    "class_FA",
+    "class_LPC",
+    "class_LPC_O",
+    "class_LPC_P",
+]
+
+score_cols = ["PRS_CCA_Component_1", "Lipid_CCA_Component_1"]
+
+for score_col in score_cols:
+    path = RESULTS_DIR / f"cca_{score_col}_regression_results.pkl"
+
+    @task(id=score_col)
+    def _(produces: Annotated[Path, Product] = path, score_col=score_col):
+        df = pd.read_pickle(BLD_DATA / "multimodal_lipid_subset_df.pkl")
+        result_df = cca_prs_lipids_regression(
+            multimodal_lipid_subset_df=df,
+            prs_cols=prs_cols,
+            lipid_cols=lipid_cols,
+            score_col=score_col,
+            n_permutations=10000,
+            random_state=42,
+        )
+        result_df.to_pickle(produces)
