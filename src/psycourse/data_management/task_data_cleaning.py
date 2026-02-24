4451155ce2c00d688e7ae45cf84eeb15b80c08b2
anthano <thanabalasingamanoja@gmail.com>
2026-02-24 11:32:13 +1100

    combined lipid enrichment plot

--- src/psycourse/data_management/task_data_cleaning.py
+++ src/psycourse/data_management/task_data_cleaning.py
@@ -67,13 +67,13 @@ prs_data_paths = {
     "bpd_data": DATA_DIR / "prs" / "final_bpd_2025-07-02_prs_collection.tsv",
     "lipid_bpd_data": DATA_DIR
     / "lipid_prs"
-    / "lipids_bpd_prs_2026-01-02_prs_collection.tsv",
+    / "lipids_bpd_prs_2026-02-06_prs_collection.tsv",
     "lipid_scz_data": DATA_DIR
     / "lipid_prs"
-    / "lipids_scz_prs_2026-01-03_prs_collection.tsv",
+    / "lipids_scz_prs_2026-02-06_prs_collection.tsv",
     "lipid_mdd_data": DATA_DIR
     / "lipid_prs"
-    / "lipids_mdd_prs_2025-12-16_prs_collection.tsv",
+    / "lipids_mdd_prs_2026-02-06_prs_collection.tsv",
 }
 
 
